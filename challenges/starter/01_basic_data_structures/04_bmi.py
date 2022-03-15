891e8f5880b3324dd45a44d41ae7cc01303d3df1
Thiha Naung <thihanaung.thnn@gmail.com>
2022-03-15 23:31:13 +0630

    adding statements_1

---
+++ challenges/starter/01_basic_data_structures/04_bmi.py
@@ -0,0 +1,21 @@
+# Write a program that calculates the Body Mass Index (BMI)
+# BMI measures a person is underweight or normal or obese
+#
+# Instructions
+# 1. get the person's weight in kilogram
+# 2. get the person's height in meter
+# 3. calculate BMI and print in 2 decimal place
+
+# formula to calculate BMI
+# BMI = weight in kilogram / square of height in meter
+
+# input should look like this
+"""
+Enter your weight in kilogram: 60
+Enter your height in meter: 1.5
+"""
+
+# output should look like this
+"""
+Your BMI is 26.67.
+"""
