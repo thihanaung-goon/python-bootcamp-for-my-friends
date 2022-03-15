5bc7df439322fa6c624547e13716a6341728ccc9
Thiha Naung <thihanaung.thnn@gmail.com>
2022-03-16 01:47:46 +0630

    update statements

---
+++ challenges/starter/01_basic_data_structures/05_life_in_days.py
@@ -0,0 +1,18 @@
+# Write a program to check how many days left to live.
+
+# 1. get the user input of his/her age
+# 2. ask the user how long is he expecting to live.
+# 3. calculate the days left to live until he expected.
+
+# assume 365 days in a year. Forget about leap year
+
+# Eg - Your input
+"""
+What is your age? >> 30
+How long are you expecting to live? >> 90
+"""
+
+# Your output
+"""
+Your have to live 21900 days left in your life.
+"""
---
+++ challenges/starter/02_statements/01_bmi_update.py
@@ -0,0 +1,35 @@
+# update the BMI program
+# 1. get the weight of the user
+# 2. ask the unit pound or kilogram
+# 3. get the height of the user
+# 4. ask the unit : foot or meter
+# 5. convert the units if needed
+# 6. formula for BMI
+# bmi = weight in kg / square of height in meter
+
+# 1 pound = 0.4536 kg
+# 1 foot = 0.3048 meter
+
+# if bmi below 18.5 => underweight
+# if bmi between 18.5 - 24.9 => healthy
+# if bmi between 25.0 - 29.9 => overweight
+# if bmi 30.0 and above => obese
+
+# 7. print the score and corresponding results
+
+
+# Your input should like this
+"""
+Enter your weight: 140
+Enter the unit: kg or lb >> lb
+Enter your height: 5.9
+Enter the unit: ft or m >> ft
+"""
+
+# Your output should like this
+"""
+Your BMI is 19.64.
+It means you are healthy
+"""
+
+# GOOD LUCK
