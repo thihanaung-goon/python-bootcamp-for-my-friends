522d39c61d17f168c36bd75e97a2d199908a7f57
Thiha Naung <thihanaung.thnn@gmail.com>
2022-03-16 11:40:34 +0630

    update statements

---
+++ challenges/starter/02_statements/02_fizzbuzz.py
@@ -0,0 +1,34 @@
+# 1. Write a program to print out number
+# if number is divisible by 3, print Fizz.
+# if the number is divisible by 5, print Buzz.
+# if the number is both divisible by both 3 and 5, print FizzBuzz.
+# 2. Ask the user to enter how many number to print.
+
+# Input Eg-
+"""
+How many times do you want to count? >> 20
+"""
+
+# Output Eg.
+"""
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+"""
---
+++ challenges/starter/02_statements/03_rock_paper_scissor.py
@@ -0,0 +1,59 @@
+# 1. Create a rock paper scissor game.
+# 2. Rock Vs Paper => Paper win
+# 3. Rock Vs Scissor => Rock win
+# 4. Paper Vs Scissor => Scissor win
+
+# Feel free to write. There are many approaches.
+
+# Hints
+# There are many approaches to write. This is my approach.
+# make a list and create random choice for computer.
+# user input 0 for rock, 1 for paper and 2 for scissor and q for quit.
+
+# here example input
+"""
+0 for rock
+1 for paper
+2 for scissor
+q for quit >> 1
+"""
+
+# example output
+"""
+====================
+COMPUTER choose scissor
+YOU choose paper
+YOU LOSE
+====================
+"""
+
+
+# Whole program until 'q'
+"""
+Choose rock, paper, scissor: 
+0 for rock
+1 for paper
+2 for scissor
+q for quit >> 1
+====================
+COMPUTER choose rock
+YOU choose paper
+YOU WIN
+====================
+Choose rock, paper, scissor: 
+0 for rock
+1 for paper
+2 for scissor
+q for quit >> 1
+====================
+COMPUTER choose rock
+YOU choose paper
+YOU WIN
+====================
+Choose rock, paper, scissor: 
+0 for rock
+1 for paper
+2 for scissor
+q for quit >> q
+====================
+"""
